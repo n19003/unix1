62507135f99adc579303b37fb65de7ab832c7159
n19003 <n19003@std.it-college.ac.jp>
2019-06-27 12:42:45 +0900

    6/27test

--- 0627/062704.sh
+++ 0627/062704.sh
@@ -1,3 +1,1 @@
 find ~ -type f 2> /dev/null | xargs du 2> /dev/null | sort -nr | head -n 5
-
-
--- 0627/062705.sh
+++ 0627/062705.sh
@@ -1,4 +1,3 @@
 start=$(date --date='2019/12/31' +'%s')
 now=$(date +'%s')
 echo $(((${start} -${now})/(24 * 60 * 60)))日
-
